de2c1c234abcd7188743dfca4a291f7a0581aacb
David Lopez <fulgore0142@gmail.com>
2024-11-27 15:12:27 -0500

    fontawesome en access

--- access.php
+++ access.php
@@ -21,6 +21,9 @@ print_r($_SESSION);
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <!-- Boxicons CSS -->
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/js/all.min.js" defer></script>
+
   <script src="https://kit.fontawesome.com/fed2435e21.js" crossorigin="anonymous"></script>
   <title>FICHA</title>
   <link rel="stylesheet" href="menu/style.css" />
