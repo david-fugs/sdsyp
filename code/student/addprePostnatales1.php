4ae7215e8bdab0483b3f27b630d053e0f948ae45
david-fugs <fulgore0142@gmail.com>
2025-03-25 17:25:40 -0500

    errores en insercion de prepostnatales

--- code/student/addprePostnatales1.php
+++ code/student/addprePostnatales1.php
@@ -1,42 +1,43 @@
     <?php
-    
+
     session_start();
-    
-    if(!isset($_SESSION['id'])){
+
+    if (!isset($_SESSION['id'])) {
         header("Location: ../../index.php");
     }
-    
+
     header("Content-Type: text/html;charset=utf-8");
     $usuario      = $_SESSION['usuario'];
     $nombre       = $_SESSION['nombre'];
     $tipo_usuario = $_SESSION['tipo_usuario'];
     $cod_dane_ie  = $_SESSION['cod_dane_ie'];
 
-?>
+    ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>FICHA</title>
-    <link href="../../css/bootstrap.min.css" rel="stylesheet">
-    <style>
-        .responsive {
-            max-width: 100%;
-            height: auto;
-        }
-    </style>
-</head>
-<body>
+    <!DOCTYPE html>
+    <html lang="es">
 
-   	<?php
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>FICHA</title>
+        <link href="../../css/bootstrap.min.css" rel="stylesheet">
+        <style>
+            .responsive {
+                max-width: 100%;
+                height: auto;
+            }
+        </style>
+    </head>
+
+    <body>
+
+        <?php
         include("../../conexion.php");
         date_default_timezone_set("America/Bogota");
         $mysqli->set_charset('utf8');
-	    if(isset($_POST['btn-update']))
-        {
+        if (isset($_POST['btn-update'])) {
             $num_doc_est                        =   $_POST['num_doc_est'];
             $fecha_dig_prePostnatales           =   date('Y-m-d h:i:s');
             $mun_dig_prePostnatales             =   $_POST['mun_dig_prePostnatales'];
@@ -51,11 +52,19 @@
             $fecha_alta_prePostnatales          =   date('Y-m-d h:i:s');
             $fecha_edit_prePostnatales          =   ('0000-00-00 00:00:00');
             $id_usu                             =   $_SESSION['id'];
+            // Validar conexión
+            if (!$mysqli) {
+                die("Error de conexión: " . mysqli_connect_error());
+            }
 
-           
             $sql = "INSERT INTO prePostnatales (num_doc_est, fecha_dig_prePostnatales, mun_dig_prePostnatales, nombre_encuestador_prePostnatales, rol_encuestador_prePostnatales, edad_madre_prePostnatales, gestacion_meses_prePostnatales, embarazo_mama_prePostnatales, lactancia_mama_prePostnatales, gateo_prePostnatales, camino_prePostnatales, fecha_alta_prePostnatales, fecha_edit_prePostnatales, id_usu) values ('$num_doc_est','$fecha_dig_prePostnatales', '$mun_dig_prePostnatales', '$nombre_encuestador_prePostnatales', '$rol_encuestador_prePostnatales', '$edad_madre_prePostnatales','$gestacion_meses_prePostnatales','$embarazo_mama_prePostnatales','$lactancia_mama_prePostnatales', '$gateo_prePostnatales', '$camino_prePostnatales', '$fecha_alta_prePostnatales', '$fecha_edit_prePostnatales','$id_usu')";
             $resultado = $mysqli->query($sql);
-
+            // Ejecutar la consulta
+            if ($mysqli->query($sql) === TRUE) {
+                echo "Registro insertado correctamente.";
+            } else {
+                echo "Error al insertar: " . $mysqli->error . "<br>Consulta: " . $sql;
+            }
 
             echo "
                 <!DOCTYPE html>
@@ -89,7 +98,8 @@
                     </html>
         ";
         }
-    ?>
+        ?>
 
-</body>
-</html>
+    </body>
+
+    </html>
