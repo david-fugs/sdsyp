1755d5a8e1e7361bf2b5ff9942dbcc0087ce1003
david-fugs <fulgore0142@gmail.com>
2025-09-02 09:31:17 -0500

    cambio access

--- access.php
+++ access.php
@@ -585,7 +585,28 @@ $mysqli->close();
       <div class="menu_content">
         <ul class="menu_items">
           <div class="menu_title menu_dahsboard"></div>
+          <?php if ($tipo_usuario == 2) { ?>
+            <li class="item">
+              <div href="#" class="nav_link submenu_item">
+                <span class="navlink_icon">
+                  <i class="fa-solid fa-bullseye"></i>
+                </span>
+                <span class="navlink">Registro art masivas </span>
+                <i class="bx bx-chevron-right arrow-left"></i>
+              </div>
+              <ul class="menu_items submenu">
+                <a href="code/contratista/seeActivities.php" class="nav_link sublink">Agregar Actividades contratista</a>
+                <a href="code/contratistaCentroVida/seeActivitiesCentroVida.php" class="nav_link sublink">Actividades Centro Vida</a>
+                <!-- <a href="code/movement/seeMovement.php" class="nav_link sublink">Movimientos</a> -->
+                <a href="code/contratista/form.php" class="nav_link sublink">Registro Actividades Masivas</a>
+                <a href="code/contratistaIndividual/form.php" class="nav_link sublink">Registro Actividades Individuales</a>
+                <a href="code/contratistaCentroVida/formCentroVida.php" class="nav_link sublink">Registros indiv Centro Vida</a>
+                <a href="code/contratistaCentroVida/formMasivoCentroVida.php" class="nav_link sublink">Registros masiva Centro Vida</a>
+                <a href="code/personMovement/seePersonMovement.php" class="nav_link sublink">Movimientos Personas</a>
 
+              </ul>
+            </li>
+            <?php } ?>
 
           <?php if ($tipo_usuario == 1) { ?>
             <li class="item">
